015368d066e8b942af70e9f638da29e8e4c7109f
surender18 <140454495+surender18@users.noreply.github.com>
2024-03-10 02:10:53 +0530

    Add files via upload
    added

---
+++ main.js
@@ -0,0 +1,48 @@
+const accessKey="D4pn-wpiJfOBhEs2Uf94Wki-86z6kev1Ku9DA4w03Ck"
+const formEl=document.querySelector("form")
+const inputE1 =document.getElementById("search-input")
+const searchResults=document.querySelector(".search-results")
+const showMore=document.getElementById("show-more-button")
+
+let inputData=""
+let page = 1;
+async function searchImages(){
+    inputData=inputE1.value;
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    const response=await fetch(url)
+    const data= await response.json()
+    const results=data.results
+
+    if(page===1){
+        searchResults.innerHTML=""
+    }
+    results.map((result)=>{
+        const imageWrapper=document.createElement('div')
+        imageWrapper.classList.add("search-result")
+        const image=document.createElement('img')
+        image.src=result.urls.small
+        image.alt=result.alt_description
+        const imageLink=document.createElement('a')
+        imageLink.href=result.links.innerHTML
+        imageLink.target="_blank"
+        imageLink.textContent=result.alt_description
+
+        imageWrapper.appendChild(image)
+        imageWrapper.appendChild(imageLink)
+        searchResults.appendChild(imageWrapper)
+
+    });
+    page++
+    if(page>1){
+        showMore.style.display="block"
+    }
+}
+formEl.addEventListener("submit",(event)=>{
+event.preventDefault()
+page=1;
+searchImages()
+})
+showMore.addEventListener("click",()=>{
+
+searchImages()
+})
